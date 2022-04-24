9774d6a8b46a3429c192c7e927f3a7d8636063c8
1710797241 <1710797241@qq.com>
2022-04-24 17:43:39 +0800

    装饰

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,25 +1,42 @@
 import React, { Suspense } from 'react';
 const RemoteApp = React.lazy(() => import('app2/App'));
 import Button from 'app2/Button';
+const withForm = config => WrappedComponent =>
+    class Demo extends React.Component {
+        render() {
+            return <WrappedComponent />;
+        }
+    };
+
+const create = params => {
+    return WrappedComponent => {
+        return class Demo extends React.Component {
+            render() {
+                return <WrappedComponent />;
+            }
+        };
+    };
+};
+
 const App = () => {
-  return (
-    <div>
-      <div
-        style={{
-          margin: '10px',
-          padding: '10px',
-          textAlign: 'center',
-          backgroundColor: 'greenyellow',
-        }}
-      >
-        <h1>App1</h1>
-        <Button />
-      </div>
-      <Suspense fallback={'loading...'}>
-        <RemoteApp />
-      </Suspense>
-    </div>
-  );
+    return (
+        <div>
+            <div
+                style={{
+                    margin: '10px',
+                    padding: '10px',
+                    textAlign: 'center',
+                    backgroundColor: 'greenyellow',
+                }}
+            >
+                <h1>App1 load</h1>
+                <Button />
+            </div>
+            <Suspense fallback={'loading...'}>
+                <RemoteApp />
+            </Suspense>
+        </div>
+    );
 };
 
 export default App;
