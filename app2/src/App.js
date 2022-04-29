0f50616f09a4b1e6df2d71da00b88adbbff8a777
wangzhongyong <wzy@kgtdata.com>
2022-04-29 16:32:49 +0800

    引入 yiyi-audio-recorder socket

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,15 +1,65 @@
 import React from 'react';
-import Socket from './utils/socket';
+import Socket from 'yiyi-websocket';
+import Recorder from 'yiyi-audio-recorder';
 const App = () => {
+    const socket = new Socket({ url: 'ws://localhost:8080' });
+    let recorder = null;
     const handleClick = () => {
-        const socket = new Socket();
-        socket.connectWebSocket();
+        console.log(
+            'socket.getStatus',
+            socket.getStatus(),
+            'socket.isCanConnect()',
+            socket.isCanConnect()
+        );
+
+        socket.connectWebSocket().then(res => {
+            console.log('connect', res);
+        });
+        socket.onopen = function (e) {
+            console.log('onOpen', e);
+        };
         socket.onmessage = function (e) {
-            console.log('收到msg', e.data, JSON.parse(e.data).name);
+            console.log('onmessage 收到msg', e.data, JSON.parse(e.data).name);
         };
         socket.onclose = function (e) {
-            console.log('连接关闭');
+            console.log('onclose', e);
         };
+        socket.onerror = function (e) {
+            console.log('onerror', e);
+        };
+    };
+    const handleSend = () => {
+        socket.sendMessage(new Date().getTime()).then(res => {
+            console.log('sendMessage', res);
+        });
+    };
+    const handleClose = () => {
+        socket.closeWebSocket();
+    };
+
+    const startRecord = () => {
+        if (!recorder) {
+            recorder = new Recorder();
+            recorder.start().then(res => {});
+        }
+    };
+
+    const stopRecord = () => {
+        recorder.stop();
+    };
+
+    const playRecord = () => {
+        recorder.play();
+    };
+
+    const getMP3Blob = () => {
+        const blob = recorder.getMP3Blob();
+        const file = new File(blob, '录音', { type: blob.type });
+        console.log('blob', blob, 'file', file);
+    };
+    const downloadMp3 = () => {
+        console.log('recorder', recorder);
+        recorder.downloadMP3();
     };
     return (
         <div
@@ -27,6 +77,56 @@ const App = () => {
                 }}
             >
                 ws-server
+            </button>
+            <button
+                onClick={() => {
+                    handleSend();
+                }}
+            >
+                send ws-server
+            </button>
+            <button
+                onClick={() => {
+                    handleClose();
+                }}
+            >
+                close ws-server
+            </button>
+
+            <button
+                onClick={() => {
+                    startRecord();
+                }}
+            >
+                开始录音
+            </button>
+            <button
+                onClick={() => {
+                    playRecord();
+                }}
+            >
+                播放录音
+            </button>
+            <button
+                onClick={() => {
+                    stopRecord();
+                }}
+            >
+                停止录音
+            </button>
+            <button
+                onClick={() => {
+                    getMP3Blob();
+                }}
+            >
+                获取mp3Blob
+            </button>
+            <button
+                onClick={() => {
+                    downloadMp3();
+                }}
+            >
+                下载Mp3
             </button>
         </div>
     );
